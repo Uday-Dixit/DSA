a6858754dbd061a4f808b79ebb6eb788590d4f73
Uday-Dixit <udaydixit420@gmail.com>
2025-08-23 19:49:20 +0530

    Leetcode 485

---
+++ Arrays/Day2_Easy/Maximum Consecutive Ones/File.java
@@ -0,0 +1,20 @@
+class File {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max_count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }else{
+                if(max_count<count){
+                    max_count=count;
+                }
+                count=0;
+            }
+        }
+        if(max_count<count){
+            max_count=count;
+        }
+        return max_count;
+    }
+}
