ff2dbb973f00ca4679c86752810d74a4cabd50af
Uday-Dixit <udaydixit420@gmail.com>
2025-08-24 17:48:42 +0530

    Leetcode 268 Optimized Solution

--- Arrays/Day2_Easy/Find missing number in an array/NewFile.java
+++ Arrays/Day2_Easy/Find missing number in an array/NewFile.java
@@ -1,4 +1,4 @@
-class Solution {
+class NewFile {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int sum = n*(n+1)/2;
---
+++ Arrays/Day2_Easy/Find missing number in an array/OptimizedFile.java
@@ -0,0 +1,9 @@
+class OptimizedFile {
+    public int missingNumber(int[] nums) {
+        int ans = nums.length;
+        for(int i=0;i<nums.length;i++){
+            ans ^= i^nums[i];
+        }
+        return ans;
+    }
+}
