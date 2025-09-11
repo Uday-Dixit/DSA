9a5d53762fb5ed0ad595eaf548cee9b07ec429d4
Uday-Dixit <udaydixit420@gmail.com>
2025-09-11 17:29:50 +0530

    LeetCode 287

---
+++ Arrays/Day3_Medium/Find the duplicate in an array of N+1 integers/File.java
@@ -0,0 +1,21 @@
+class File {
+    public int findDuplicate(int[] nums) {
+        int low=1;
+        int high=nums.length-1;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            int count=0;
+            for(int i:nums){
+                if(i<=mid){
+                    count++;
+                }
+            }
+            if(count>mid){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
